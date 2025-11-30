d1f91bdba77d3f84b01e88667b5d67d56a0389ee
dotrunic <beyer.michael.mb@gmail.com>
2025-11-30 22:34:21 +0100

    add snack(sidebar) config

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      hidden = true,
+      ignored = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
+}
